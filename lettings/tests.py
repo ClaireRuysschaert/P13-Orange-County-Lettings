b55ab1f0b9fedfc6343ec04b384ba5ea5870302f
ClaireRuysschaert <netclaire@hotmail.fr>
2024-08-02 15:09:11 +0200

    feat: Add tests for all the models and views

--- lettings/tests.py
+++ lettings/tests.py
@@ -1,3 +1,174 @@
-from django.test import TestCase  # noqa: F401
+from django.test import TestCase
+from django.urls import reverse
+from lettings.models import Address, Letting
 
-# Create your tests here.
+
+class ModelTestCase(TestCase):
+    """ "
+    Test case for the models in the `lettings` app.
+    This test case contains test methods for the creation and deletion of instances of the
+    `Address` and `Letting` models.
+    Attributes:
+      NUMBER (int): The number of the address.
+      STREET (str): The street of the address.
+      CITY (str): The city of the address.
+      STATE (str): The state of the address.
+      ZIP_CODE (int): The zip code of the address.
+      COUNTRY_ISO_CODE (int): The ISO code of the country.
+      TITLE (str): The title of the letting.
+      INVALID_NUMBER (str): An invalid number for testing.
+      WRONG_ADDRESS (str): An invalid address for testing.
+      address (Address): An instance of the `Address` model.
+      letting (Letting): An instance of the `Letting` model.
+    """
+
+    NUMBER = 2
+    STREET = "Place de la Comédie"
+    CITY = "Bordeaux"
+    STATE = "Gironde"
+    ZIP_CODE = 33000
+    COUNTRY_ISO_CODE = 250
+    TITLE = "Test Title Letting"
+    INVALID_NUMBER = "rue"
+    WRONG_ADDRESS = "No model instance"
+
+    def setUp(self) -> None:
+        """
+        Set up the test case with an Address and a letting.
+        """
+        self.address = Address.objects.create(
+            number=self.NUMBER,
+            street=self.STREET,
+            city=self.CITY,
+            state=self.STATE,
+            zip_code=self.ZIP_CODE,
+            country_iso_code=self.COUNTRY_ISO_CODE,
+        )
+        self.letting = Letting.objects.create(title=self.TITLE, address=self.address)
+
+    def test_address_creation_success(self) -> None:
+        """
+        Test the successful creation of an instance of the `lettings.Address` model.
+        Asserts that the created instance has the expected attributes.
+        """
+        self.assertEqual(self.address.number, self.NUMBER)
+        self.assertEqual(self.address.street, self.STREET)
+        self.assertEqual(self.address.city, self.CITY)
+
+    def test_address_creation_fail(self) -> None:
+        """
+        Test the failed creation of an instance of the `lettings.Address` model.
+        Asserts that a ValueError is raised when an invalid number is used.
+
+        :raises ValueError: If an invalid number is used for creating an instance
+        of the `lettings.Address` model.
+        """
+        with self.assertRaises(ValueError):
+            Address.objects.create(
+                number=self.INVALID_NUMBER,
+                street=self.STREET,
+                city=self.CITY,
+                state=self.STATE,
+                zip_code=self.ZIP_CODE,
+                country_iso_code=self.COUNTRY_ISO_CODE,
+            )
+
+    def test_address_delete_success(self) -> None:
+        """
+        Test the successful deletion of an instance of the `lettings.Address` model.
+        Verify that the instance is deleted from the database.
+        """
+        self.assertEqual(Address.objects.all().count(), 1)
+        self.address.delete()
+        self.assertEqual(Address.objects.all().count(), 0)
+
+    def test_letting_create_success(self) -> None:
+        """
+        Test the successful creation of an instance of the `lettings.Letting` model.
+        Asserts that the created instance has the expected attributes.
+        """
+        self.assertEqual(self.letting.title, self.TITLE)
+        self.assertEqual(self.letting.address.number, self.NUMBER)
+        self.assertEqual(self.letting.address.street, self.STREET)
+        self.assertEqual(self.letting.address.city, self.CITY)
+        self.assertEqual(self.letting.address.state, self.STATE)
+
+    def test_letting_create_fail(self) -> None:
+        """
+        Test the failed creation of an instance of the `lettings.Letting` model.
+        Asserts that a ValueError is raised when an invalid address is used.
+
+        :raises ValueError: If an invalid address is used for creating an instance
+        of the `lettings.Letting` model.
+        """
+        with self.assertRaises(ValueError):
+            Letting.objects.create(title=self.TITLE, address=self.WRONG_ADDRESS)
+
+    def test_letting_delete_success(self) -> None:
+        """
+        Test the successful deletion of an instance of the `lettings.Letting` model.
+        Verify that the instance is deleted from the database.
+        """
+        self.assertEqual(Letting.objects.all().count(), 1)
+        self.letting.delete()
+        self.assertEqual(Letting.objects.all().count(), 0)
+
+
+class LettingsViewsTestCase(TestCase):
+    """
+    Test case for the views in the Lettings app.
+    """
+
+    NUMBER = 2
+    STREET = "Place de la Comédie"
+    CITY = "Bordeaux"
+    STATE = "Gironde"
+    ZIP_CODE = 33000
+    COUNTRY_ISO_CODE = 250
+    TITLE = "Test Title Letting"
+
+    def setUp(self) -> None:
+        """
+        Set up the necessary objects for each test case.
+        """
+        self.address = Address.objects.create(
+            number=self.NUMBER,
+            street=self.STREET,
+            city=self.CITY,
+            state=self.STATE,
+            zip_code=self.ZIP_CODE,
+            country_iso_code=self.COUNTRY_ISO_CODE,
+        )
+        self.letting = Letting.objects.create(title=self.TITLE, address=self.address)
+
+    def test_index_view(self) -> None:
+        """
+        Test the index view.
+        """
+        response = self.client.get(reverse("lettings:lettings_index"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, self.letting.title)
+        self.assertTemplateUsed(response, "lettings/index.html")
+
+    def test_letting_view(self) -> None:
+        """
+        Test the letting view.
+        """
+        response = self.client.get(reverse("lettings:letting", args=[self.letting.id]))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, self.letting.title)
+        self.assertContains(response, self.letting.address.number)
+        self.assertContains(response, self.letting.address.street)
+        self.assertContains(response, self.letting.address.city)
+        self.assertContains(response, self.letting.address.state)
+        self.assertTemplateUsed(response, "letting.html")
+
+    def test_letting_view_not_found(self) -> None:
+        """
+        Test the letting view when the letting is not found.
+        """
+        response = self.client.get(
+            reverse("lettings:letting", args=[self.letting.pk + 1])
+        )
+        self.assertEqual(response.status_code, 404)
+        self.assertTemplateUsed(response, "404.html")
--- profiles/tests.py
+++ profiles/tests.py
@@ -1,3 +1,164 @@
-from django.test import TestCase  # noqa: F401
+from django.db import IntegrityError
+from django.test import TestCase
 
-# Create your tests here.
+from profiles.models import Profile
+from django.contrib.auth import get_user_model
+
+UserModel = get_user_model()
+
+
+class ModelTestCase(TestCase):
+    """
+    Test case for the UserModel and Profile model.
+
+    Attributes:
+        USERNAME (str): The username of the user.
+        MAIL (str): The email of the user.
+        PASSWORD (str): The password of the user.
+        FIRST_NAME (str): The first name of the user.
+        LAST_NAME (str): The last name of the user.
+        FAVORITE_CITY (str): The favorite city of the user.
+        user (User): An instance of the UserModel.
+        profile (Profile): An instance of the Profile
+    """
+
+    USERNAME = "Test User"
+    MAIL = "test@test.com"
+    PASSWORD = "testpassword"
+    FIRST_NAME = "Test"
+    LAST_NAME = "User"
+    FAVORITE_CITY = "Paris"
+
+    def setUp(self) -> None:
+        """
+        Set up the test case with a user and profile.
+        """
+        self.user = UserModel.objects.create_user(
+            username=self.USERNAME,
+            email=self.MAIL,
+            password=self.PASSWORD,
+            first_name=self.FIRST_NAME,
+            last_name=self.LAST_NAME,
+        )
+        self.profile = Profile.objects.create(
+            user=self.user, favorite_city=self.FAVORITE_CITY
+        )
+
+    def test_user_creation_success(self) -> None:
+        """
+        Test that a user is created successfully with the correct attributes.
+        """
+        self.assertEqual(self.user.username, self.USERNAME)
+        self.assertEqual(self.user.email, self.MAIL)
+        self.assertEqual(self.user.first_name, self.FIRST_NAME)
+        self.assertEqual(self.user.last_name, self.LAST_NAME)
+
+    def test_user_creation_fail(self) -> None:
+        """
+        Test that creating a user with empty fields raises a ValueError.
+        """
+        with self.assertRaises(ValueError):
+            UserModel.objects.create_user(
+                username="",
+                email="",
+                password="",
+                first_name="",
+                last_name="",
+            )
+
+    def test_user_cannot_be_created_multiple_times(self) -> None:
+        """
+        Test that creating a user with the same username, email, and password
+        raises an IntegrityError.
+        """
+        with self.assertRaises(IntegrityError):
+            UserModel.objects.create_user(
+                username=self.USERNAME,
+                password=self.PASSWORD,
+                email=self.MAIL,
+                first_name=self.FIRST_NAME,
+                last_name=self.LAST_NAME,
+            )
+
+    def test_user_delete_successful(self) -> None:
+        """
+        Test that a user is deleted successfully.
+        """
+        self.assertEqual(UserModel.objects.all().count(), 1)
+        self.user.delete()
+        self.assertEqual(UserModel.objects.all().count(), 0)
+
+    def test_profile_creation_success(self) -> None:
+        """
+        Test that a profile is created successfully with the correct attributes.
+        """
+        self.assertEqual(self.profile.favorite_city, self.FAVORITE_CITY)
+        self.assertEqual(self.profile.user, self.user)
+
+    def test_profile_delete_successful(self) -> None:
+        """
+        Test that a profile is deleted successfully.
+        """
+        self.assertEqual(Profile.objects.all().count(), 1)
+        self.profile.delete()
+        self.assertEqual(Profile.objects.all().count(), 0)
+
+
+class ProfilesViewsTestCase(TestCase):
+    """
+    Test case for the profile views.
+    """
+
+    USERNAME = "Test User"
+    MAIL = "test@test.com"
+    PASSWORD = "testpassword"
+    FIRST_NAME = "Test"
+    LAST_NAME = "User"
+    FAVORITE_CITY = "Paris"
+
+    def setUp(self) -> None:
+        """
+        Set up the test case with a user and profile.
+        """
+        self.user = UserModel.objects.create_user(
+            username=self.USERNAME,
+            email=self.MAIL,
+            password=self.PASSWORD,
+            first_name=self.FIRST_NAME,
+            last_name=self.LAST_NAME,
+        )
+        self.profile = Profile.objects.create(
+            user=self.user, favorite_city=self.FAVORITE_CITY
+        )
+
+    def test_profiles_index_view(self) -> None:
+        """
+        Test that the profiles index view returns a 200 status code
+        and contains the correct user information.
+        """
+        response = self.client.get("/profiles/")
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, self.profile.user.username)
+        self.assertTemplateUsed(response, "profiles/index.html")
+
+    def test_profile_detail_view(self) -> None:
+        """
+        Test that the profile detail view returns a 200 status code
+        and contains the correct profile information.
+        """
+        response = self.client.get(f"/profiles/{self.profile.pk}/")
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, self.profile.favorite_city)
+        self.assertContains(response, self.profile.user.username)
+        self.assertContains(response, self.profile.user.email)
+        self.assertContains(response, self.profile.user.first_name)
+        self.assertContains(response, self.profile.user.last_name)
+        self.assertTemplateUsed(response, "profile.html")
+
+    def test_profile_detail_view_not_found(self) -> None:
+        """
+        Test that the profile detail view returns a 404 status code when the profile is not found.
+        """
+        response = self.client.get(f"/profiles/{self.profile.pk+1}/")
+        self.assertEqual(response.status_code, 404)
+        self.assertTemplateUsed(response, "404.html")
