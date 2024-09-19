d72c97c0a8684440bb58d1dc2d861279d0d2a00e
ClaireRuysschaert <netclaire@hotmail.fr>
2024-09-19 16:12:37 +0200

    style: clean flake 8

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -88,7 +88,7 @@ WSGI_APPLICATION = "oc_lettings_site.wsgi.application"
 # }
 DATABASES = {
     'default': dj_database_url.config(default={os.getenv('DATABASE_URL')}, conn_max_age=600)
-}       
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
