c59daef93b70b3e20ec9e29c7e6873859a8d79e7
ClaireRuysschaert <netclaire@hotmail.fr>
2024-09-19 16:23:37 +0200

    style: cleaning up requirements.txt

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -87,7 +87,7 @@ WSGI_APPLICATION = "oc_lettings_site.wsgi.application"
 #     }
 # }
 DATABASES = {
-    'default': dj_database_url.config(default={os.getenv('DATABASE_URL')}, conn_max_age=600)
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'), conn_max_age=600)
 }
 
 # Password validation
