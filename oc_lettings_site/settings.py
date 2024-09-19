e97d7c72ebfa26630f4e24d4a6cd9b6d6699cab8
ClaireRuysschaert <netclaire@hotmail.fr>
2024-09-19 16:05:19 +0200

    feat: allow all hosts

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'https://oc-lettings-site-fkk6.onrender.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '*']
 
 
 # Application definition
