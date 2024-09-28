7eb4ae8939e87cf26e78e5696f9727dab43a13ef
ClaireRuysschaert <netclaire@hotmail.fr>
2024-09-28 16:29:37 +0200

    fix: Add missing files to .dockerignore

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -25,10 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.getenv("SECRET_KEY", "abc")
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "*"]
-
 
 # Application definition
 
@@ -130,6 +129,10 @@ STATIC_URL = "/static/"
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+if os.getenv("DEBUG") == "True":
+    DEBUG = True
+else:
+    DEBUG = False
 
 STORAGES = {
     "staticfiles": {
