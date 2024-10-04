6c2eb55ce470161bd1f46d5b5bb74b5e2624d3a4
ClaireRuysschaert <netclaire@hotmail.fr>
2024-10-04 11:14:41 +0200

    Add read the docs documentation settings

---
+++ docs/source/conf.py
@@ -0,0 +1,45 @@
+import os
+import sys
+
+
+# Chemin absolu vers le répertoire du projet Django
+sys.path.insert(0, os.path.abspath(".."))
+
+# Configuration de l'environnement Django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "oc_lettings_site.settings")
+
+
+# -- Project information
+
+project = 'oc-lettings-site'
+copyright = '2024, Ruysschaert'
+author = 'Ruysschaert'
+
+release = '0.1'
+version = '0.1.0'
+
+# -- General configuration
+
+extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
+    "sphinx.ext.viewcode",
+]
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+}
+intersphinx_disabled_domains = ['std']
+
+templates_path = ['_templates']
+
+# -- Options for HTML output
+
+html_theme = 'sphinx_rtd_theme'
+
+# -- Options for EPUB output
+epub_show_urls = 'footnote'
